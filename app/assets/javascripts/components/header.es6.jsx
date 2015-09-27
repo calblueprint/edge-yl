e5ebb7635b4f1ccd9b340f182a190f4fe91dd5ac
Anthony <anthonyhbh@berkeley.edu>
2015-09-26 19:56:45 -0700

    Style header component

--- app/assets/javascripts/components/header.es6.jsx
+++ app/assets/javascripts/components/header.es6.jsx
@@ -1,11 +1,25 @@
 class Header extends React.Component {
 
-  render() {
-    return (
-      <div>
-        HI
-      </div>
-    );
-  }
+	get styles() {
+		return {
+			container: {
+				height: 48,
+				backgroundColor: '#c3e2f4',
+			},
+			title: {
+				color: '#ffffff',
+			},
+		};
+	}
+
+	render() {
+		return (
+			<div style={this.styles.container}>
+				<span style={this.styles.title}>
+					{'edge-yl'}
+				</span>
+			</div>
+		);
+	}
 }
 
