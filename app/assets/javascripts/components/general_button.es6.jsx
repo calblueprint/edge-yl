4e21156dc3cb0effd018aa4ea2386458ed8c3649
warrenshen <warrenzshen@gmail.com>
2015-10-04 12:28:58 -0700

    Configure general button component

--- app/assets/javascripts/components/general_button.es6.jsx
+++ app/assets/javascripts/components/general_button.es6.jsx
@@ -1,6 +1,43 @@
 class GeneralButton extends React.Component {
+
+  static get propTypes() {
+    return {
+      action: React.PropTypes.func,
+      route: React.PropTypes.string,
+      content: React.PropTypes.string.isRequired,
+    };
+  }
+
+  static get defaultProps() {
+    return {
+      action: null,
+      route: '',
+      content: '',
+    };
+  }
+
+  get styles() {
+    
+  }
+  
+  handleClick(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    if (this.props.route !== '') {
+      // Route
+    } else if (this.props.action !== null) {
+      this.props.action();
+    }
+  }
+
   render () {
-    return <div />;
+    return (
+      <a
+        href={this.props.route}
+        onClick={this.handleClick.bind(this)}>
+        {this.props.content}
+      </a>
+    );
   }
 }
 
