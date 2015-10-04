b3182ecefabfe7663a397017e883634500ffe27d
warrenshen <warrenzshen@gmail.com>
2015-10-03 21:00:44 -0700

    Fix general styling of profile grid

--- app/assets/javascripts/components/profile_card.es6.jsx
+++ app/assets/javascripts/components/profile_card.es6.jsx
@@ -3,8 +3,14 @@ class ProfileCard extends React.Component {
   get styles() {
     return {
       container: {
+        display: 'inline-block',
         position: 'relative',
-        width: 300,
+        width: 172,
+        padding: 24,
+        backgroundColor: 'white',
+        border: '1px solid',
+        borderColor: '#e5e6e9 #dfe0e4 #d0d1d5',
+        borderRadius: 3,
       },
     };
   }
@@ -12,7 +18,7 @@ class ProfileCard extends React.Component {
   render () {
     return (
       <div style={this.styles.container}>
-        profile card!
+        <span>profile card!</span>
       </div>
     );
   }
