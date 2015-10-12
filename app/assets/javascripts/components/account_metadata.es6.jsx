ec65b007327132397b82c072d69956a9ac8f2a58
sonia.yang <soniayang97@gmail.com>
2015-10-12 00:38:32 -0700

    Fix spacing and add style to main content div

---
+++ app/assets/javascripts/components/account_metadata.es6.jsx
@@ -0,0 +1,45 @@
+class AccountMetadata extends Component {
+
+  get styles() {
+    return {
+      container: {
+        display: 'flex',
+        flexDirection: 'column',
+        position: 'relative',
+        alignItems: 'center',
+        width: '100%',
+        borderBottom: 'solid #D6D6D6 3px',
+      },
+      image: {
+        borderRadius: '50%',
+        overflow: 'hidden',
+        width: '250px',
+        height: 'auto',
+        marginTop: '76px',
+      },
+      name: {
+        paddingTop: '24px',
+        paddingBottom:'16px',
+        fontSize: '32px',
+      }, 
+      position: {
+        fontSize: '16px',
+        paddingBottom: '20px',
+        fontStyle: 'italic',
+      },
+    };
+  }
+
+  render() {
+    return (
+      <div style={this.styles.container}>
+        <img 
+          src='https://scontent.fsnc1-1.fna.fbcdn.net/hphotos-xfp1/t31.0-8/11856297_10200932572512494_2256826043885795533_o.jpg' 
+          style={this.styles.image} /> 
+        <span style={this.styles.name} >Anthony Huang</span>
+        <span style={this.styles.position}>Volunteer, Recruitment Group</span>
+      </div>
+    );
+  }
+}
+
---
+++ app/assets/javascripts/components/account_page.es6.jsx
@@ -0,0 +1,61 @@
+class AccountPage extends Component {
+
+  static get defaultState() {
+    return { sidebar: true };
+  }
+
+  get styles() {
+    return {
+      container: {
+        display: 'flex',
+        flexDirection: 'column',
+        position: 'relative',
+        width: '100vw',
+        height: '100vh',
+      },
+      body: {
+        flex: 1,
+        position: 'relative',
+        display: 'flex', 
+        flexDirection: 'row',
+        justifyContent: 'center',
+        width: '100%',
+
+      },
+      content: {
+        marginLeft: '236px', 
+        position: 'relative',
+        display: 'flex',
+        width: '712px',
+        height: '100%',
+        backgroundColor: 'white',
+        border: '1px solid',
+        borderColor: '#e5e6e9 #dfe0e4 #d0d1d5',
+        borderRadius: '3px',
+        marginTop: '30px',
+        padding: '0px 45px',
+      },
+    };
+  }
+
+  handleClick(event) {
+    this.setState({ sidebar: !this.state.sidebar });
+  }
+
+  render() {
+    return (
+      <div style={this.styles.container}>
+        <Header
+          handleSidebarClick={this.handleClick.bind(this)} />
+        <div style={this.styles.body}>
+          <Sidebar shouldShow={this.state.sidebar} />
+          <div style = {this.styles.content}> 
+            <AccountMetadata />
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
