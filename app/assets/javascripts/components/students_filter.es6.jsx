13f1214e7899c23f75f00e4f51a02ebfaa245c3a
warrenshen <warrenzshen@gmail.com>
2015-10-14 11:14:32 -0700

    Generate initial `StudentsFilter` component

---
+++ app/assets/javascripts/components/students_filter.es6.jsx
@@ -0,0 +1,24 @@
+class StudentsFilter extends Component {
+
+  get styles() {
+    return {
+      container: {
+        position: 'relative',
+        padding: '24px',
+        marginTop: '2%',
+        backgroundColor: 'white',
+        border: '1px solid',
+        borderColor: '#e5e6e9 #dfe0e4 #d0d1d5',
+        borderRadius: '3px',
+      },
+    };
+  }
+
+  render() {
+    return (
+      <div style={this.styles.container}>
+
+      </div>
+    );
+  }
+}
