bc00de60d3ffc1f98f2de0f16a713f271d327f12
warrenshen <warrenzshen@gmail.com>
2015-10-15 21:02:34 -0700

    Generate temporary students data

--- app/assets/javascripts/components/students_page.es6.jsx
+++ app/assets/javascripts/components/students_page.es6.jsx
@@ -32,6 +32,42 @@ class StudentsPage extends Component {
     };
   }
 
+  // TODO(Warren): Remove this temporary method after our api is ready.
+  get students() {
+    return [
+      {
+        age: 16,
+        birthday: '1/1/1999',
+        email: 'katelyn_marchan@gmail.com',
+        first_name: 'Katelyn',
+        last_name: 'Marchan',
+        phone_number: '(408) 436-7654',
+        school: 'School One',
+        status: 'Pending',
+      },
+      {
+        age: 15,
+        birthday: '12/25/2000',
+        email: 'jonie_distefano@gmail.com',
+        first_name: 'Jonie',
+        last_name: 'Distefano',
+        phone_number: '(510) 333-3333',
+        school: 'School Two',
+        status: 'Attending',
+      },
+      {
+        age: 16,
+        birthday: '11/11/1999',
+        email: 'melva_currence@gmail.com',
+        first_name: 'Melva',
+        last_name: 'Currence',
+        phone_number: '(888) 555-5555',
+        school: 'School Three',
+        status: 'Pending',
+      },
+    ];
+  }
+
   handleClick(event) {
     this.setState({ sidebar: !this.state.sidebar });
   }
