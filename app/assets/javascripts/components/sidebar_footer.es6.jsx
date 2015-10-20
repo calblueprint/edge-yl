b9b7a2eaa4ab8f1d240302ea1868da34b0d49388
sonia.yang <soniayang97@gmail.com>
2015-10-19 17:22:09 -0700

    Change blueprint logo to clickable

--- app/assets/javascripts/components/sidebar_footer.es6.jsx
+++ app/assets/javascripts/components/sidebar_footer.es6.jsx
@@ -6,16 +6,6 @@ class SidebarFooter extends Component {
 
   get styles() {
     return {
-      container: {
-        display: 'flex',
-        flex: '1', 
-        flexDirection: 'row',
-        position: 'relative',
-        padding: '20px',
-        marginLeft: '14px',
-        alignItems: 'center',
-        
-      },
       text: {
         height: '100%',
         paddingRight: '5px',
@@ -29,14 +19,30 @@ class SidebarFooter extends Component {
     };
   }
 
+  get clickableStyles() {
+    return { 
+      default: { 
+        display: 'flex',
+        flex: '1', 
+        flexDirection: 'row',
+        position: 'relative',
+        padding: '20px',
+        marginLeft: '14px',
+        alignItems: 'center',
+      }
+    };
+  }
+
   render() {
     return (
-      <div style={this.styles.container}> 
+      <Clickable
+         styles={this.clickableStyles}
+         route={RouteConstants.students.index}> 
         <span style={this.styles.text}>Powered by</span>
         <img
           src='https://avatars3.githubusercontent.com/u/2729578?v=3&s=200'
           style={this.styles.img}/>
-      </div>
+      </Clickable>
     );
   }
 }
