63ac9956501cc85c8a7adfc5c85f65375373105c
warrenshen <warrenzshen@gmail.com>
2015-10-20 01:45:22 -0700

    Convert RoutesConstants to singleton

--- app/assets/javascripts/constants/route_constants.es6.js
+++ app/assets/javascripts/constants/route_constants.es6.js
@@ -1,12 +1,22 @@
-var RouteConstants = {
-  pages: {
-    login: '/login',
-    signup: '/signup',
-    profile: '/profile',
-    mail: '/mail',
-  },
-  students: {
-    index: '/students',
-    show: '/students/1',
-  },
-};
+'use strict';
+
+class RoutesSingleton {
+
+  get pages() {
+    return {
+      login: '/login',
+      signup: '/signup',
+      profile: '/profile',
+      mail: '/mail',
+    };
+  }
+
+  get students() {
+    return {
+      index: '/students',
+      show: '/students/1',
+    };
+  }
+}
+
+var RouteConstants = new RoutesSingleton();
