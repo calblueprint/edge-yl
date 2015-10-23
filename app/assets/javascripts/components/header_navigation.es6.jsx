2e821ecf2bc74f80246d0c2823c1397b4dffed40
Max Wolffe <max.alan.wolffe@gmail.com>
2015-10-22 21:32:24 -0700

    Refactor Header Middle Section into Header Navigation

---
+++ app/assets/javascripts/components/header_navigation.es6.jsx
@@ -0,0 +1,43 @@
+class HeaderNavigation extends Component {
+
+  get styles() {
+    return {
+      container: {
+        display: 'flex',
+        flex: '1',
+        padding: '0 12px',
+        alignItems: 'center',
+        alignSelf: 'stretch',
+      },
+      icon: {
+        position: 'relative',
+        color: 'white',
+        fontSize: '20px',
+      },
+    };
+  }
+
+  get clickableStyles() {
+    return {
+      default: {
+        position: 'relative',
+        color: 'white',
+        paddingRight: '16px',
+        fontColor: 'white',
+        fontSize: '24px',
+      },
+    };
+  }
+
+  render() {
+    return (
+      <div style={this.styles.container}>
+        <Clickable
+          content={'EDGE'}
+          route={RouteConstants.pages.login}
+          styles={this.clickableStyles} />
+        <HeaderSearch />
+      </div>
+    );
+  }
+}
