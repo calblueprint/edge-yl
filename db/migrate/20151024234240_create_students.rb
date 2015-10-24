bd292f5b502a6b19715d7c1993d0f9141dfc0edb
warrenshen <warrenzshen@gmail.com>
2015-10-24 16:49:22 -0700

    Create student model

---
+++ db/migrate/20151024234240_create_students.rb
@@ -0,0 +1,16 @@
+class CreateStudents < ActiveRecord::Migration
+  def change
+    create_table :students do |t|
+
+      t.date :birthday, null: false
+      t.string :cell_phone, null: false
+      t.string :email, null: false
+      t.string :first_name, null: false
+      t.string :home_address, null: false
+      t.string :home_phone, null: false
+      t.string :last_name, null: false
+
+      t.timestamps null: false
+    end
+  end
+end
