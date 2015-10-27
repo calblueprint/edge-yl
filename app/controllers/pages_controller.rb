0c667435ce443118bba3f2eff011cf5a1ff60a03
warrenshen <warrenzshen@gmail.com>
2015-10-27 16:38:15 -0700

    Render signup and login routes with devise controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,5 @@
 class PagesController < ApplicationController
 
-	def login
-    @is_login = true
-    render 'authentication'
-	end
-
-  def signup
-    @is_login = false
-    render 'authentication'
-  end
-
   def mail
   end
 
