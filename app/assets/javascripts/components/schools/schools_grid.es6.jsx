934ad6d572bf5e59df5339a2f8fb7463957acfed
Max Wolffe <max.alan.wolffe@gmail.com>
2015-10-29 17:00:34 -0700

    Add schools page and schools card component

---
+++ app/assets/javascripts/components/schools/schools_grid.es6.jsx
@@ -0,0 +1,46 @@
+class SchoolsGrid extends Component {
+
+  static get propTypes() {
+    return {
+      students: React.PropTypes.array.isRequired,
+    };
+  }
+
+  static get defaultProps() {
+    return {
+      students: [],
+    };
+  }
+
+  get styles() {
+    return {
+      container: {
+        display: 'flex',
+        flexFlow: 'wrap',
+        justifyContent: 'space-between',
+        alignContent: 'flex-start',
+        flex: '1',
+      },
+    };
+  }
+
+  renderCard(school) {
+    return (
+      <SchoolsCard
+        key={school.id}
+        school={school} />
+    );
+  }
+
+  renderCards() {
+    return this.props.students.map(this.renderCard.bind(this));
+  }
+
+  render() {
+    return (
+      <div style={this.styles.container}>
+        {this.renderCards()}
+      </div>
+    );
+  }
+}
