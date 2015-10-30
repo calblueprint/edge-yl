2ecad3fce34611e4873d1363d63395f899b8d280
Unzi Park <upark.go@gmail.com>
2015-10-29 22:39:02 -0700

    Seed data for Schools model

---
+++ db/seeds/development/1_schools.rb
@@ -0,0 +1,9 @@
+(1..25).each do |n|
+  new_school = School.create(
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    counselor_email: Faker::Internet.email,
+    counselor_name: "#{Faker::Name.first_name}, #{Faker::Name.last_name}",
+    name: "#{Faker::Name.first_name} Highschool",
+  )
+  puts "Created school: #{new_school.first_name}"
+end
