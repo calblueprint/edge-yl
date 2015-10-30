57d7ad6d7168be9d29958c171c4f509e791ee101
Unzi Park <upark.go@gmail.com>
2015-10-29 23:12:17 -0700

    Set up views for schools

--- app/assets/javascripts/components/schools/schools_card.es6.jsx
+++ app/assets/javascripts/components/schools/schools_card.es6.jsx
@@ -51,7 +51,7 @@ static get propTypes() {
     return (
       <div style={this.styles.container}>
         <img
-          src='https://scontent.fsnc1-1.fna.fbcdn.net/hphotos-xfp1/t31.0-8/11856297_10200932572512494_2256826043885795533_o.jpg'
+          src='http://www.wlac.edu/WLAC/media/images/highschool/highschool-index.jpg'
           style={this.styles.image} />
         <div style={this.styles.info}>
           <Clickable
