5de87526945722e2b2971816989d8e688798ba19
warrenshen <warrenzshen@gmail.com>
2015-11-04 20:25:00 -0800

    Fix configuration for devise users

--- db/seeds/development/0_users.rb
+++ db/seeds/development/0_users.rb
@@ -1,6 +1,5 @@
 new_admin = User.create(
   birthday: Faker::Date.between(33.days.ago, Date.today),
-  cell_phone: Faker::PhoneNumber.phone_number,
   email: Faker::Internet.email,
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
@@ -13,7 +12,6 @@ puts "Created admin: #{new_admin.first_name} #{new_admin.last_name}"
 (1..3).each do |n|
   new_volunteer = User.create(
     birthday: Faker::Date.between(33.days.ago, Date.today),
-    cell_phone: Faker::PhoneNumber.phone_number,
     email: Faker::Internet.email,
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
