02778962f2b8c282be8922d6b614da0caafd9b0e
warrenshen <warrenzshen@gmail.com>
2015-11-08 10:55:46 -0800

    Fix line spacing of controllers

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,5 @@
 class RegistrationsController < Devise::RegistrationsController
+
   respond_to :json
 
   def create
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < Devise::SessionsController
+
   respond_to :json
 
   def create
