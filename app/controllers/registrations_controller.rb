b0078378365b210bfe29b60d35cb7578463d3c8c
warrenshen <warrenzshen@gmail.com>
2015-11-07 19:01:53 -0800

    Create user registration create action

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
   respond_to :json
+
+  def create
+    user = User.new user_params
+    if user.save
+      sign_in(user)
+      render json: user, status: 201
+    else
+      error_response(user)
+    end
+  end
+
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,9 +2,7 @@ class StudentsController < ApplicationController
 
   def create
     student = Student.new student_params
-    respond_to do |format|
-      format.json { render json: student }
-    end
+    render json: student
   end
 
   def index
