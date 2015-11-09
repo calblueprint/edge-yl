492948d5685863d9dec7fcabc8b1e3340f452c27
sonia.yang <soniayang97@gmail.com>
2015-11-09 01:16:32 -0800

    Add ref to components

--- app/assets/javascripts/components/authentication/signup_form.es6.jsx
+++ app/assets/javascripts/components/authentication/signup_form.es6.jsx
@@ -31,40 +31,48 @@ class SignupForm extends Component {
   createUser(event) {
     var params = {
       registration: {
-        email: 'warrenshen@berkeley.edu',
-        birthday: '2015-1-1',
-        first_name: 'Warren',
-        last_name: 'Shen',
-        password: 'password',
-        password_confirmation: 'password',
+        email: this.state.email,
+        birthday: this.state.birthday,
+        first_name: this.state.first_name,
+        last_name: this.state.last_name,
+        password: this.state.password,
+        password_confirmation: this.state.password_confirmation,
       },
     };
     var resolve = (response) => { console.log('Sign up success'); window.location = RouteConstants.students.index };
-    var reject = (response) => { console.log(response) };
+    var reject = (response) => { console.log(response); this.setState({errors: true}) };
     Requester.post(RouteConstants.users.create, params, resolve, reject);
   }
 
-  componentDidMount() {
-    var email = ReactDOM.findDOMNode(this.refs.email); 
-    email.addEventListener('input', this.generateHandler('email'));
-    var birthday = ReactDOM.findDOMNode(this.refs.email); 
-    birthday.addEventListener('input', this.generateHandler('birthday'));
-    var firstName = ReactDOM.findDOMNode(this.refs.email); 
-    firstName.addEventListener('input', this.generateHandler('first_name'));
-    var lastName = ReactDOM.findDOMNode(this.refs.email); 
-    lastName.addEventListener('input', this.generateHandler('last_name'));
-    var password = ReactDOM.findDOMNode(this.refs.email); 
-    password.addEventListener('input', this.generateHandler('password'));
-    var passwordConfirmation = ReactDOM.findDOMNode(this.refs.email); 
-    passwordConfirmation.addEventListener('input', this.generateHandler('password_confirmation')); 
+  generateHandler(field) {
+    var state = {};
+    return function(event) {
+      state[field] = event.target.value;
+      this.setState(state);
+    }.bind(this);
   }
 
   renderMessage() {
     return (
       <span style={this.styles.error}>
-        {'Invalid email/password'}
+        {'Invalid input'}
       </span>
     );
+  }
+ 
+  componentDidMount() {
+    var email = ReactDOM.findDOMNode(this.refs.email); 
+    email.addEventListener('input', this.generateHandler('email'));
+    var birthday = ReactDOM.findDOMNode(this.refs.birthday); 
+    birthday.addEventListener('input', this.generateHandler('birthday'));
+    var firstName = ReactDOM.findDOMNode(this.refs.firstName); 
+    firstName.addEventListener('input', this.generateHandler('first_name'));
+    var lastName = ReactDOM.findDOMNode(this.refs.lastName); 
+    lastName.addEventListener('input', this.generateHandler('last_name'));
+    var password = ReactDOM.findDOMNode(this.refs.password); 
+    password.addEventListener('input', this.generateHandler('password'));
+    var passwordConfirmation = ReactDOM.findDOMNode(this.refs.passwordConfirmation); 
+    passwordConfirmation.addEventListener('input', this.generateHandler('password_confirmation')); 
   }
 
   // TODO: Make Birthday into dropdown 
@@ -77,6 +85,7 @@ class SignupForm extends Component {
         <input
           autoFocus={true}
           placeholder={'example@email.com'}
+          ref={'email'}
           style={this.styles.input}>
         </input>
         <label style={this.styles.label}>
@@ -85,6 +94,7 @@ class SignupForm extends Component {
         <input
           placeholder={'topsecretpassword'}
           style={this.styles.input}
+          ref={'password'}
           type={'password'}>
         </input>
         <label style={this.styles.label}>
@@ -93,6 +103,7 @@ class SignupForm extends Component {
         <input
           placeholder={'topsecretpassword'}
           style={this.styles.input}
+          ref={'passwordConfirmation'}
           type={'password'}>
         </input>
         <label style={this.styles.label}>
@@ -100,6 +111,7 @@ class SignupForm extends Component {
         </label>
         <input
           placeholder={'Emily'}
+          ref={'firstName'}
           style={this.styles.input}>
         </input>
         <label style={this.styles.label}>
@@ -107,6 +119,7 @@ class SignupForm extends Component {
         </label>
         <input
           placeholder={'Wilson'}
+          ref={'lastName'}
           style={this.styles.input}>
         </input>
         <label style={this.styles.label}>
@@ -114,6 +127,7 @@ class SignupForm extends Component {
         </label>
         <input
           placeholder={'01/22/95'}
+          ref={'birthday'}
           style={this.styles.input}>
         </input>
         {this.state.errors && this.renderMessage()}
