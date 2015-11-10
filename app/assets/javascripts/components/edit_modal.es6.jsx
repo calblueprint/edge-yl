5a5aafee0050c0948de3832cb490f9dfadb8feeb
Unzi Park <upark.go@gmail.com>
2015-11-09 21:38:15 -0800

    Create EditModal component

---
+++ app/assets/javascripts/components/edit_modal.es6.jsx
@@ -0,0 +1,51 @@
+class EditModal extends Component {
+    static get propTypes() {
+      return {
+        student: React.PropTypes.object.isRequired,
+        type: React.PropTypes.oneOf([
+          'contact',
+          'conference',
+          'parent',
+          'preview',
+        ]).isRequired,
+      };
+    }
+
+  static get defaultProps() {
+    return {
+      student: {},
+      type: 'preview',
+    };
+  }
+
+  get styles() {
+    return {
+      container: Object.assign(
+        {},
+        StyleConstants.cards.defaults,
+        {
+          display: 'flex',
+          flexFlow: 'column',
+          width: '32.5%',
+          height: '312px',
+          marginTop: '1%',
+        }
+      ),
+    };
+  }
+
+  renderBody() {
+    switch (this.props.type) {
+      default:
+        return <EditPreview {...this.props} />;
+    };
+  }
+
+  render() {
+    return (
+      <div style={this.styles.container}>
+        {this.renderBody()}
+      </div>
+    );
+  }
+}
