2d9ca6e0536cbe379b4ef6cb13b130bac754404a
Max Wolffe <max.alan.wolffe@gmail.com>
2015-11-09 21:33:08 -0800

    Start student_comments controller

---
+++ app/controllers/api/students/comments_controller.rb
@@ -0,0 +1,14 @@
+class Api::Students::CommentsController < Api::BaseController
+    # TODO (warren) Check if we should extend ApiBaseController
+
+    def create
+    end
+
+    def index
+    end
+
+    def update
+    end
+
+
+end
