c12af8e75bcfce1dc86aaccb229609f74783dc16
Anthony <anthonyhbh@gmail.com>
2015-11-10 02:35:45 -0800

    Add Create and Update for User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,27 @@ class UsersController < ApplicationController
     end
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :birthday)
+    end
+
 end
