79766298d5e271b5833aa9cb4363cac8744693f5
Anthony <anthonyhbh@gmail.com>
2015-11-10 21:24:56 -0800

    Fix Create/Update in UserController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,19 +12,10 @@ class UsersController < ApplicationController
     end
   end
 
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to @user
-    else
-      render 'new'
-    end
-  end
-
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      redirect_to @user
+      render json: user
     else
       render 'edit'
     end
