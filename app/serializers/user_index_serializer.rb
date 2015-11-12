8a7e3d04bf4776572408234f4ba74662b0393d69
Max Wolffe <max.alan.wolffe@gmail.com>
2015-11-11 22:45:39 -0800

    Add create action and coment_params to the comment_controller

--- app/serializers/user_index_serializer.rb
+++ app/serializers/user_index_serializer.rb
@@ -1,6 +1,6 @@
 class UserIndexSerializer < BaseSerializer
 
-  attributes :id, :first_name, :last_name, :birthday, :is_admin, :created_at
+  attributes :id, :first_name, :last_name, :is_admin, :created_at
 
   has_many :comments
 
