5b56449f569cf48a039ce552f7ec00d2059a056e
warrenshen <warrenzshen@gmail.com>
2015-11-11 23:12:35 -0800

    Refactor error_response convenience methods to Api::BaseController

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -2,4 +2,22 @@ class Api::BaseController < ApplicationController
 
   respond_to :json
 
+  def error_response(object: nil, message: nil, status: nil)
+    render json: { message: response_message(object, message) }, status: status
+  end
+
+  def unprocessable_response(object)
+    error_response object: object, status: 422
+  end
+
+  def unauthorized_response
+    error_response message: 'Unauthorized', status: 403
+  end
+
+  private
+
+  def response_message(object=nil, message=nil)
+    message || object.errors.full_messages.to_sentence
+  end
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,22 +2,4 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :null_session
 
-  def error_response(object: nil, message: nil, status: nil)
-    render json: { message: response_message(object, message) }, status: status
-  end
-
-  def unprocessable_response(object)
-    error_response object: object, status: 422
-  end
-
-  def unauthorized_response
-    error_response message: 'Unauthorized', status: 403
-  end
-
-  private
-
-  def response_message(object=nil, message=nil)
-    message || object.errors.full_messages.to_sentence
-  end
-
 end
