9f971fc2f8effa0a361c288dd16fafe004b8e582
sonia.yang <soniayang97@gmail.com>
2015-11-11 18:34:15 -0800

    Set selected as clicked element

--- app/assets/javascripts/components/form/form_dropdown.es6.jsx
+++ app/assets/javascripts/components/form/form_dropdown.es6.jsx
@@ -1,37 +1,53 @@
 class FormDropdown extends Component { 
 
-	static get propTypes() {
-		return {
-			options: React.PropTypes.array.isRequired,
-			title: React.PropTypes.string.isRequired,
+  static get propTypes() {
+    return {
+      options: React.PropTypes.array.isRequired,
+      title: React.PropTypes.string.isRequired,
+    };
+  }
 
-		};
-	}
+  static get defaultState() { 
+    return { 
+      dropdown: false,
+      value: 'Select one', 
+    };
+  }
+  
+  handleClick(event) {
+    this.setState({ dropdown: !this.state.dropdown });
+  }
 
-	static get defaultState() { 
-		return { 
-			dropdown: false,
-			value: 'Select one', 
-		};
-	}
-	
-	handleClick(event) {
-		this.setState({ dropdown: !this.state.dropdown });
-	}
 
-	renderDropdown() {
-		console.log('options' + this.props.options);
-		if (this.state.dropdown) {
-			console.log('hi');
+  itemSelected(content) {
+    console.log('content'+ content);
+    this.setState({value: content, dropdown: false}); 
 
-			console.log(this.styles.dropdown);
-			return (
-				<Dropdown
-					styles={this.dropdownStyles}
-					options={this.props.options} />
-			);
-		}
-	}
+  }
+
+  get dropdownOptions() { 
+    for (var i = 0; i<this.props.options.length; i++) {
+      console.log(i);
+      content = this.props.options[i].content
+      console.log(content);
+
+      this.props.options[i].func = () => {this.itemSelected(content)};
+      console.log(this.props.options[i]);
+    }
+
+    return this.props.options;
+  }
+
+  renderDropdown() {
+    if (this.state.dropdown) {
+      return (
+        <Dropdown
+          styles={this.dropdownStyles}
+          options={this.dropdownOptions} 
+          />
+      );
+    }
+  }
 
   get dropdownStyles() {
     return {
@@ -46,7 +62,6 @@ class FormDropdown extends Component {
       },
       container: Object.assign(
         {},
-        
         {
           display: 'flex',
           flexFlow: 'column',      
@@ -57,63 +72,64 @@ class FormDropdown extends Component {
   }
 
   get clickableStyles() { 
-  	return {
-  		default: {
-  			padding: '0px 2px',
-  		},
-  		hover: {
-
-  		},
-  	};
+    return {
+      default: {
+        padding: '0px 2px',
+      },
+      hover: {
+      },
+    };
   }
-	get styles() {
-		return {
-			container: {
-				display: 'flex', 
-				flexFlow: 'row',
-			},
-			label: {
-				paddingRight: '24px',
-				paddingTop: '4px',
-				fontSize: StyleConstants.fonts.sizes.smaller,
-			},
-			selected: {
-				width: '130px',
-				color: StyleConstants.colors.gray,
-			},
-			dropdownContainer: {
-				display: 'flex',
-				flexFlow: 'row',
-				padding: '4px',
-				border: '1px solid' + StyleConstants.colors.gray,
-			},
-			icon: {
-				padding: '0px 2px',
-			},
-			dropdown: {
-				display: 'flex',
-				flexFlow: 'column',
-			}
-		};
-	}
 
-	render() {
-		return (
-			<div style = {this.styles.container}>
-				<label style={this.styles.label}>{this.props.title}</label>
-					<div style={this.styles.dropdownContainer}> 
-						<div style = {this.styles.dropdown}> 
-							<span style = {this.styles.selected} > {this.state.value} </span>
-							{this.renderDropdown()}
-						</div>
-						<Clickable
-							icon={'fa fa-angle-down'}
-							func={this.handleClick.bind(this)}
-							type={'i'}
-							styles={this.clickableStyles}
-						/>
-					</div>
-			</div>	
-		);
-	}
+  get styles() {
+    return {
+      container: {
+        display: 'flex', 
+        flexFlow: 'row',
+      },
+      label: {
+        paddingRight: '24px',
+        paddingTop: '4px',
+        fontSize: StyleConstants.fonts.sizes.smaller,
+      },
+      selected: {
+        width: '130px',
+        color: StyleConstants.colors.gray,
+      },
+      dropdownContainer: {
+        display: 'flex',
+        flexFlow: 'row',
+        padding: '4px',
+        border: '1px solid' + StyleConstants.colors.gray,
+      },
+      icon: {
+        padding: '0px 2px',
+        color: StyleConstants.colors.gray,
+      },
+      dropdown: {
+        display: 'flex',
+        flexFlow: 'column',
+      },
+    };
+  }
+
+  render() {
+    return (
+      <div style = {this.styles.container}>
+        <label style={this.styles.label}>{this.props.title}</label>
+          <div style={this.styles.dropdownContainer}> 
+            <div style = {this.styles.dropdown}> 
+              <span style = {this.styles.selected}> {this.state.value} </span>
+              {this.renderDropdown()}
+            </div>
+            <Clickable
+              icon={'fa fa-angle-down'}
+              func={this.handleClick.bind(this)}
+              type={'i'}
+              styles={this.clickableStyles}
+            />
+          </div>
+      </div>  
+    );
+  }
 }
