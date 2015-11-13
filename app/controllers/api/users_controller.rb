59d9dc191b4a8ac76a58a78ad04dbde560dd0982
Anthony <anthonyhbh@gmail.com>
2015-11-13 00:33:34 -0800

    Fix styles

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,22 +1,22 @@
 class Api::UsersController < Api::BaseController
 
   def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
-      render json: @user, serializer: UserIndexSerializer, status: 201
+    user = User.find(params[:id])
+    if user.update_attributes(user_params)
+      render json: user, serializer: UserIndexSerializer, status: 201
     else
-      error_response object: @user, status: 401
+      unprocessable_response: user
     end
   end
 
   private
     def user_params
       params.require(:user).permit(
+        :email, 
         :first_name, 
         :last_name, 
-        :email, 
         :password
-        )
+      )
     end
 
 end
