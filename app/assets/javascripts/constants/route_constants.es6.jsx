85a53524db8e80b8af1ace929ac9ee05c28526ff
warrenshen <warrenzshen@gmail.com>
2015-11-14 00:09:19 -0800

    Refactor constants into implicit singletons

--- app/assets/javascripts/constants/route_constants.es6.jsx
+++ app/assets/javascripts/constants/route_constants.es6.jsx
@@ -1,41 +1,40 @@
-'use strict';
+(() => {
+  class RouteConstants {
 
-class RoutesSingleton {
+    get pages() {
+      return {
+        login: '/login',
+        signup: '/signup',
+        profile: '/profile',
+        mail: '/mail',
+      };
+    }
 
-  get pages() {
-    return {
-      login: '/login',
-      signup: '/signup',
-      profile: '/profile',
-      mail: '/mail',
-    };
+    get schools() {
+      return {
+        index: '/schools',
+        show: function(id) {
+          return `/schools/${id}`;
+        },
+      };
+    }
+
+    get students() {
+      return {
+        index: function(page) {
+          return `/students?page=${page}`;
+        },
+        show: function(id) {
+          return `/students/${id}`;
+        },
+      };
+    }
+
+    get users() {
+      return {
+        index: '/users',
+      };
+    }
   }
-
-  get schools() {
-    return {
-      index: '/schools',
-      show: function(id) {
-        return `/schools/${id}`;
-      },
-    };
-  }
-
-  get students() {
-    return {
-      index: function(page) {
-        return `/students?page=${page}`;
-      },
-      show: function(id) {
-        return `/students/${id}`;
-      },
-    };
-  }
-
-  get users() {
-    return {
-      index: '/users',
-    };
-  }
-}
-
-var RouteConstants = new RoutesSingleton();
+  this.RouteConstants = new RouteConstants();
+})();
