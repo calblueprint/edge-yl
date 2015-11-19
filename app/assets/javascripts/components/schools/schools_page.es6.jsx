830893e535cf972baec668af243500e275ba2622
sonia.yang <soniayang97@gmail.com>
2015-11-19 00:16:35 -0800

    Delete space after styles divider

--- app/assets/javascripts/components/schools/schools_page.es6.jsx
+++ app/assets/javascripts/components/schools/schools_page.es6.jsx
@@ -11,7 +11,6 @@ class SchoolsPage extends Component {
   // --------------------------------------------------
   // Styles
   // --------------------------------------------------
-
   get styles() {
     return {
       container: {
