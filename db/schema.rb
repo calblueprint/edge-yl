b7f432f8cb6864543551d1802a62fc55e2ec00b0
Max Wolffe <max.alan.wolffe@gmail.com>
2015-11-20 21:37:26 -0800

    Add pg_search migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151121050717) do
+ActiveRecord::Schema.define(version: 20151121052736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_trgm"
+  enable_extension "fuzzystrmatch"
 
   create_table "comments", force: :cascade do |t|
     t.text     "content",    null: false
