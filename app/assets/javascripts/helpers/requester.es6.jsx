94672f662dbbf0741011ab74eaec4268dff56861
warrenshen <warrenzshen@gmail.com>
2015-11-22 13:31:01 -0800

    Refactor requester module with arrow syntax

--- app/assets/javascripts/helpers/requester.es6.jsx
+++ app/assets/javascripts/helpers/requester.es6.jsx
@@ -1,75 +1,74 @@
-'use strict';
+(() => {
+  class Requester {
 
-class RequesterSingleton {
-
-  delete(route, resolve, reject) {
-    var request = this.initialize('DELETE', route);
-    request.onreadystatechange = function() {
-      if (this.readyState === XMLHttpRequest.DONE) {
-        if (this.status === 204) {
-          if (resolve) {
-            resolve();
+    delete(route, resolve, reject) {
+      var request = this.initialize('DELETE', route);
+      request.onreadystatechange = () => {
+        if (request.readyState === XMLHttpRequest.DONE) {
+          if (request.status === 204) {
+            if (resolve) {
+              resolve();
+            }
           }
         }
-      }
-    };
-    request.send();
-  }
+      };
+      request.send();
+    }
 
-  get(route, resolve, reject) {
-    var request = this.initialize('GET', route);
-    request.onreadystatechange = function() {
-      if (this.readyState === XMLHttpRequest.DONE) {
-        if (this.status === 200) {
-          if (resolve) {
-            resolve(JSON.parse(this.response));
+    get(route, resolve, reject) {
+      var request = this.initialize('GET', route);
+      request.onreadystatechange = () => {
+        if (request.readyState === XMLHttpRequest.DONE) {
+          if (request.status === 200) {
+            if (resolve) {
+              resolve(JSON.parse(request.response));
+            }
           }
         }
-      }
-    };
-    request.send();
-  }
+      };
+      request.send();
+    }
 
-  initialize(type, route) {
-    var request = new XMLHttpRequest();
-    request.open(type, route);
-    request.setRequestHeader('Accept', 'application/json');
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'));
-    return request;
-  }
+    initialize(type, route) {
+      var request = new XMLHttpRequest();
+      request.open(type, route);
+      request.setRequestHeader('Accept', 'application/json');
+      request.setRequestHeader('Content-Type', 'application/json');
+      request.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'));
+      return request;
+    }
 
-  post(route, params, resolve, reject) {
-    var request = this.initialize('POST', route);
-    request.onreadystatechange = function() {
-      if (this.readyState === XMLHttpRequest.DONE) {
-        if (this.status === 201) {
-          if (resolve) {
-            resolve(JSON.parse(this.response));
-          }
-        } else if (this.status === 401 || this.status === 422) {
-          if (reject) {
-            reject(JSON.parse(this.response));
-          }
-        }
-      }
-    };
-    request.send(JSON.stringify(params));
-  }
-
-  update(route, params, resolve) {
-    var request = this.initialize('PATCH', route);
-    request.onreadystatechange = function() {
-      if (this.readyState === XMLHttpRequest.DONE) {
-        if (this.status === 201) {
-          if (resolve) {
-            resolve(JSON.parse(this.response));
+    post(route, params, resolve, reject) {
+      var request = this.initialize('POST', route);
+      request.onreadystatechange = () => {
+        if (request.readyState === XMLHttpRequest.DONE) {
+          if (request.status === 201) {
+            if (resolve) {
+              resolve(JSON.parse(request.response));
+            }
+          } else if (request.status === 401 || request.status === 422) {
+            if (reject) {
+              reject(JSON.parse(request.response));
+            }
           }
         }
-      }
-    };
-    request.send(JSON.stringify(params));
-  }
-}
+      };
+      request.send(JSON.stringify(params));
+    }
 
-var Requester = new RequesterSingleton();
+    update(route, params, resolve) {
+      var request = this.initialize('PATCH', route);
+      request.onreadystatechange = () => {
+        if (request.readyState === XMLHttpRequest.DONE) {
+          if (request.status === 201) {
+            if (resolve) {
+              resolve(JSON.parse(request.response));
+            }
+          }
+        }
+      };
+      request.send(JSON.stringify(params));
+    }
+  }
+  this.Requester = new Requester();
+})();
