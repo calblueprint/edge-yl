62d0e02f75f947ab4097a580a75dabc656c6ce34
warrenshen <warrenzshen@gmail.com>
2015-11-25 00:31:48 -0800

    Fix student factory and seed

--- db/seeds/development/3_students.rb
+++ db/seeds/development/3_students.rb
@@ -1,9 +1,15 @@
 (1..50).each do |index|
-  new_student = Student.create(
+  new_student = Student.create!(
     birthday: Faker::Date.between(33.days.ago, Date.today),
     cell_phone: Faker::PhoneNumber.phone_number,
     email: Faker::Internet.email,
     first_name: Faker::Name.first_name,
+    guardian_one_name: Faker::Name.name,
+    guardian_one_phone: Faker::PhoneNumber.phone_number,
+    guardian_one_email: Faker::Internet.email,
+    guardian_two_name: Faker::Name.name,
+    guardian_two_phone: Faker::PhoneNumber.phone_number,
+    guardian_two_email: Faker::Internet.email,
     home_address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
     home_phone: Faker::PhoneNumber.phone_number,
     last_name: Faker::Name.last_name,
--- spec/factories/students.rb
+++ spec/factories/students.rb
@@ -28,12 +28,12 @@ FactoryGirl.define do
     cell_phone          { Faker::PhoneNumber.phone_number }
     email               { Faker::Internet.email }
     first_name          { Faker::Name.first_name }
-    guardian_one_email  { Faker::Internet.email}
-    guardian_one_name   { Faker::Name.name}
-    guardian_one_phone  { Faker::PhoneNumber.phone_number}
-    guardian_two_email  { Faker::Internet.email}
-    guardian_two_name   { Faker::Name.name}
-    guardian_two_phone  { Faker::PhoneNumber.phone_number}
+    guardian_one_email  { Faker::Internet.email }
+    guardian_one_name   { Faker::Name.name }
+    guardian_one_phone  { Faker::PhoneNumber.phone_number }
+    guardian_two_email  { Faker::Internet.email }
+    guardian_two_name   { Faker::Name.name }
+    guardian_two_phone  { Faker::PhoneNumber.phone_number }
     home_address        { "#{Faker::Address.street_address}, #{Faker::Address.city}" }
     home_phone          { Faker::PhoneNumber.phone_number }
     last_name           { Faker::Name.last_name }
