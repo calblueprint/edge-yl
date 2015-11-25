e3e811b76aa56627186cd46df328fcc040adac07
sonia.yang <soniayang97@gmail.com>
2015-11-24 23:06:40 -0800

    Add guardian attributes to school migration

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,17 +2,23 @@
 #
 # Table name: students
 #
-#  id           :integer          not null, primary key
-#  birthday     :date             not null
-#  cell_phone   :string           not null
-#  email        :string           not null
-#  first_name   :string           not null
-#  home_address :string           not null
-#  home_phone   :string           not null
-#  last_name    :string           not null
-#  school_id    :integer
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
+#  id                 :integer          not null, primary key
+#  birthday           :date             not null
+#  cell_phone         :string           not null
+#  email              :string           not null
+#  first_name         :string           not null
+#  guardian_one_name  :string           not null
+#  guardian_one_phone :string           not null
+#  guardian_one_email :string           not null
+#  guardian_two_name  :string           not null
+#  guardian_two_phone :string           not null
+#  guardian_two_email :string           not null
+#  home_address       :string           not null
+#  home_phone         :string           not null
+#  last_name          :string           not null
+#  school_id          :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
 #
 
 class Student < ActiveRecord::Base
