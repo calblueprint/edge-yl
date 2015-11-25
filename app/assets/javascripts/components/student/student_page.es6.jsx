bfd984c082d9b94656271e50cb859fbb31a5ece6
warrenshen <warrenzshen@gmail.com>
2015-11-24 21:43:38 -0800

    Refactor full_name model method name

--- app/assets/javascripts/components/student/student_page.es6.jsx
+++ app/assets/javascripts/components/student/student_page.es6.jsx
@@ -73,7 +73,6 @@ class StudentPage extends Component {
   }
 
   render() {
-    console.log(this.state.profile.visits);
     return (
       <div style={StyleConstants.pages.default}>
         {this.renderOverlay()}
