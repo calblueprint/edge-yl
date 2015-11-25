7d97d4123bbf83168cdb03b70bcc3b9c08fdbbba
Unzi Park <upark.go@gmail.com>
2015-11-24 22:01:24 -0800

    Update seeding data

--- db/seeds/development/6_responsibilities.rb
+++ db/seeds/development/6_responsibilities.rb
@@ -1,10 +1,12 @@
-def generate_responsibility
+def generate_responsibility(student_id)
   new_responsibility = Responsibility.create(
     status: "completed",
+    student_id: student_id,
+    user_id: 1 + rand(4),
   )
 end
 
-User.all.each do |user|
-  generate_responsibility()
-  puts "Created responsibility for #{user.first_name} #{user.last_name}."
+Student.all.each do |student|
+  generate_responsibility(student.id)
+  puts "Created responsibility for #{student.first_name} #{student.last_name}."
 end
