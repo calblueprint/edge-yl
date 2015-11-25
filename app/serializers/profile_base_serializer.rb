daef37eb817a56ba8d8370552dbfda886850d8db
warrenshen <warrenzshen@gmail.com>
2015-11-24 22:58:47 -0800

    Create profile base serializer specifically for me response

---
+++ app/serializers/profile_base_serializer.rb
@@ -0,0 +1,11 @@
+class ProfileBaseSerializer < BaseSerializer
+
+  attributes :id, :email, :first_name, :is_admin, :last_name
+
+  has_many :visits, serializer: VisitBaseSerializer
+
+  def visits
+    object.visits.first(3)
+  end
+
+end
