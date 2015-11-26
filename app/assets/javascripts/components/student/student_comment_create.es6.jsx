457d95b2b35863a082401a9b39a0fe922cb6f4c1
warrenshen <warrenzshen@gmail.com>
2015-11-26 13:53:32 -0800

    Style comment create component

--- app/assets/javascripts/components/student/student_comment_create.es6.jsx
+++ app/assets/javascripts/components/student/student_comment_create.es6.jsx
@@ -22,7 +22,7 @@ class StudentCommentCreate extends Component {
   // --------------------------------------------------
   static get defaultState() {
     return {
-      comment: null,
+      content: '',
     };
   }
 
@@ -31,12 +31,16 @@ class StudentCommentCreate extends Component {
   // --------------------------------------------------
   get styles() {
     return {
-      container: {
-        display: 'flex',
-        flexFlow: 'column',
-        justifyContent: 'center',
-        width: '356px',
-      },
+      container: Object.assign(
+        {},
+        StyleConstants.cards.default,
+        {
+          display: 'flex',
+          flexFlow: 'column',
+          justifyContent: 'center',
+          width: '356px',
+        }
+      ),
       form: {
         display: 'flex',
         flexFlow: 'column',
@@ -83,7 +87,7 @@ class StudentCommentCreate extends Component {
         <div style={this.styles.form}>
           <CardInput
             action={this.generateHandler('content')}
-            placeholder={'Your Comment Here...'}
+            placeholder={'Your comment here...'}
             value={''} />
         </div>
       </div>
