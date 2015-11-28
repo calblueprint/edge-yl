1c5e8031412c1f9f74574bd3c01b2f731778248b
warrenshen <warrenzshen@gmail.com>
2015-11-27 18:06:42 -0800

    Create form actions with alt

---
+++ app/assets/javascripts/actions/form_actions.es6.jsx
@@ -0,0 +1,13 @@
+(() => {
+  class FormActions {
+
+    storeResponse(section, question, value) {
+      return {
+        question: question,
+        section: section,
+        value: value,
+      };
+    }
+  }
+  this.FormActions = alt.createActions(FormActions);
+})();
--- app/assets/javascripts/stores/form_store.es6.jsx
+++ app/assets/javascripts/stores/form_store.es6.jsx
@@ -8,17 +8,26 @@
           questions: [
             {
               label: 'First Name',
+              placeholder: 'Kira',
               type: 'field',
               value: '',
             },
             {
               label: 'Last Name',
+              placeholder: 'Klapper',
               type: 'field',
               value: '',
             },
           ],
         },
       ];
+      this.bindListeners({
+        handleStoreResponse: FormActions.STORE_RESPONSE,
+      });
+    }
+
+    handleStoreResponse(response) {
+      console.log(response);
     }
   }
   this.FormStore = alt.createStore(FormStore);
