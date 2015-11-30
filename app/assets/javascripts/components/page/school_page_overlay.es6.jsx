526591968bd81299f0ed70a3602d235c4e68c231
sonia.yang <soniayang97@gmail.com>
2015-11-29 22:22:54 -0800

    Create new overlay template

--- app/assets/javascripts/components/page/school_page_overlay.es6.jsx
+++ app/assets/javascripts/components/page/school_page_overlay.es6.jsx
@@ -1,12 +1,4 @@
-class SchoolPageOverlay extends Component {
-
-  // --------------------------------------------------
-  // Setup
-  // --------------------------------------------------
-  constructor(props) {
-    super(props);
-    this._node = null;
-  }
+class SchoolPageOverlay extends Overlay {
 
   // --------------------------------------------------
   // Props
@@ -31,55 +23,6 @@ class SchoolPageOverlay extends Component {
       },
       school: {},
     };
-  }
-
-  // --------------------------------------------------
-  // Styles
-  // --------------------------------------------------
-  get styles() {
-    return {
-      container: {
-        display: 'flex',
-        justifyContent: 'center',
-        position: 'fixed',
-        top: '0px',
-        left: '0px',
-        zIndex: StyleConstants.planes.nine,
-        width: '100vw',
-        height: '100vh',
-        backgroundColor: StyleConstants.colors.fog,
-      },
-    };
-  }
-
-  get clickableStyles() {
-    return {
-      default: {
-        position: 'fixed',
-        top: '0px',
-        right: '0px',
-        zIndex: StyleConstants.planes.nine,
-        padding: '8px',
-      },
-    };
-  }
-
-  // --------------------------------------------------
-  // Lifecycle
-  // --------------------------------------------------
-  componentDidMount() {
-    var node = ReactDOM.findDOMNode(this.refs.container);
-    this._node = node;
-    node.addEventListener('click', (event) => this.handleClick(event));
-  }
-
-  // --------------------------------------------------
-  // Handlers
-  // --------------------------------------------------
-  handleClick(event) {
-    if (event.target === this._node) {
-      SchoolActions.storeOverlay(false);
-    }
   }
 
 //  --------------------------------------------------
---
+++ app/assets/javascripts/templates/overlay.es6.jsx
@@ -0,0 +1,62 @@
+class Overlay extends Component {
+
+  // --------------------------------------------------
+  // Setup
+  // --------------------------------------------------
+  constructor(props) {
+    super(props);
+    this._node = null;
+  }
+
+  // --------------------------------------------------
+  // Styles
+  // --------------------------------------------------
+  get styles() {
+    return {
+      container: {
+        display: 'flex',
+        justifyContent: 'center',
+        position: 'fixed',
+        top: '0px',
+        left: '0px',
+        zIndex: StyleConstants.planes.nine,
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: StyleConstants.colors.fog,
+      },
+    };
+  }
+
+    get clickableStyles() {
+    return {
+      default: {
+        position: 'fixed',
+        top: '0px',
+        right: '0px',
+        zIndex: StyleConstants.planes.nine,
+        padding: '8px',
+      },
+    };
+  }
+
+
+  // --------------------------------------------------
+  // Lifecycle
+  // --------------------------------------------------
+  componentDidMount() {
+    var node = ReactDOM.findDOMNode(this.refs.container);
+    this._node = node;
+    node.addEventListener('click', (event) => this.handleClick(event));
+  }
+
+
+  // --------------------------------------------------
+  // Handlers
+  // --------------------------------------------------
+  handleClick(event) {
+    if (event.target === this._node) {
+      SchoolActions.storeOverlay(false);
+    }
+  }
+
+}
