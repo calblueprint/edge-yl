ff699178eb3976ff73b75aeef4e2004cee2ec001
warrenshen <warrenzshen@gmail.com>
2015-12-06 13:37:41 -0800

    Update student contact edit to new edit infrastructure

--- app/assets/javascripts/components/student/student_contact_edit.es6.jsx
+++ app/assets/javascripts/components/student/student_contact_edit.es6.jsx
@@ -6,6 +6,7 @@ class StudentContactEdit extends Component {
   static get propTypes() {
     return {
       student: React.PropTypes.object.isRequired,
+      template: React.PropTypes.object.isRequired,
     };
   }
 
@@ -14,44 +15,15 @@ class StudentContactEdit extends Component {
   // --------------------------------------------------
   get styles() {
     return {
-      container: Object.assign(
-        {},
-        StyleConstants.cards.default,
-        {
-          display: 'flex',
-          flexFlow: 'column',
-          justifyContent: 'center',
-          width: '356px',
-        }
-      ),
-      form: {
+      container: {
         display: 'flex',
         flexFlow: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        flex: '1',
         padding: '18px',
         marginBottom: '18px',
       },
-      image: {
-        width: '152px',
-        height: '152px',
-        marginTop: '18px',
-        borderRadius: '50%',
-      },
     };
-  }
-
-  // --------------------------------------------------
-  // Lifecycle
-  // --------------------------------------------------
-  componentWillMount() {
-    this.setState({
-      cellPhone: this.props.student.cell_phone,
-      email: this.props.student.email,
-      homeAddress: this.props.student.home_address,
-      homePhone: this.props.student.home_phone,
-    });
   }
 
   // --------------------------------------------------
@@ -60,52 +32,33 @@ class StudentContactEdit extends Component {
   generateHandler(field) {
     var state = {};
     return(event) => {
-      state[field] = event.target.value;
-      this.setState(state);
+      StudentActions.storeTemplate(field, event.target.value);
     };
-  }
-
-  updateStudent() {
-    var params = {
-      student: {
-        cell_phone: this.state.cellPhone,
-        email: this.state.email,
-        home_address: this.state.homeAddress,
-        home_phone: this.state.homePhone,
-      },
-    };
-    StudentActions.updateStudent(this.props.student.id, params);
   }
 
   // --------------------------------------------------
   // Render
   // --------------------------------------------------
   render() {
-    var student = this.props.student;
+    var template = this.props.template;
     return (
       <div style={this.styles.container}>
-        <CardHeader
-          action={() => this.updateStudent()}
-          content={'Student Information'}
-          icon={TypeConstants.icons.save} />
-        <div style={this.styles.form}>
-          <CardInput
-            action={this.generateHandler('cellPhone')}
-            placeholder={'Cell phone'}
-            value={this.state.cellPhone} />
-          <CardInput
-            action={this.generateHandler('homePhone')}
-            placeholder={'Home phone'}
-            value={this.state.homePhone} />
-          <CardInput
-            action={this.generateHandler('email')}
-            placeholder={'Email'}
-            value={this.state.email} />
-          <CardInput
-            action={this.generateHandler('homeAddress')}
-            placeholder={'Home address'}
-            value={this.state.homeAddress} />
-        </div>
+        <CardInput
+          action={this.generateHandler('email')}
+          placeholder={'Email'}
+          value={template.email} />
+        <CardInput
+          action={this.generateHandler('cell_phone')}
+          placeholder={'Cell phone'}
+          value={template.cell_phone} />
+        <CardInput
+          action={this.generateHandler('home_phone')}
+          placeholder={'Home phone'}
+          value={template.home_phone} />
+        <CardInput
+          action={this.generateHandler('home_address')}
+          placeholder={'Home address'}
+          value={template.home_address} />
       </div>
     );
   }
