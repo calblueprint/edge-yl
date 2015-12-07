df3227166dc4b6285a3045ca9dfb5907ba7441a0
Unzi Park <upark.go@gmail.com>
2015-12-07 14:12:14 -0800

    Show registration_status on StudentsCard

--- app/serializers/student_index_serializer.rb
+++ app/serializers/student_index_serializer.rb
@@ -1,5 +1,7 @@
 class StudentIndexSerializer < StudentBaseSerializer
 
+  attributes :registration_status
+
   has_one :group, serializer: GroupStudentSerializer
   has_one :school, serializer: SchoolBaseSerializer
 
