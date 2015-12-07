b5b7f997156358ca1e37f2b114ec1f976dee9c78
Unzi Park <upark.go@gmail.com>
2015-12-07 15:26:03 -0800

    Fix syntax

--- app/serializers/student_base_serializer.rb
+++ app/serializers/student_base_serializer.rb
@@ -1,6 +1,6 @@
 class StudentBaseSerializer < BaseSerializer
 
   attributes :id, :birthday, :email,
-             :first_name, :gender, :last_name,:home_address, :shirt_size
+             :first_name, :gender, :last_name, :home_address, :shirt_size
 
 end
