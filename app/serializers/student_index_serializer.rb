6d62be2fab32e103aa5d8a5cb50ffd74a3e9edc3
sonia.yang <soniayang97@gmail.com>
2015-12-07 13:32:06 -0800

    Add attributes to student base serializer

--- app/serializers/student_index_serializer.rb
+++ app/serializers/student_index_serializer.rb
@@ -2,6 +2,6 @@ class StudentIndexSerializer < StudentBaseSerializer
 
   has_one :group, serializer: GroupStudentSerializer
   has_one :school, serializer: SchoolBaseSerializer
-
+ 	attributes :is_flagged, :is_primary
 
 end
