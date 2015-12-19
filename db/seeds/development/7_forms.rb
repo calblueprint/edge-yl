2740a5193138336f4858a9e09487bb0c354be72e
warrenshen <warrenzshen@gmail.com>
2015-12-19 11:17:40 -0800

    Populate student form seed

--- db/seeds/development/7_forms.rb
+++ db/seeds/development/7_forms.rb
@@ -1,13 +1,11 @@
-form_params = {
+new_form = Form.create(
   target: Form.targets[:student],
   title: 'Student Form',
-}
-new_form = Form.create(form_params) do |form|
-  section_params = {
+) do |form|
+  Section.create(
     form: form,
     title: 'Basic Information',
-  }
-  Section.create(section_params) do |section|
+  ) do |section|
     Question.create(
       key: 'first_name',
       placeholder: 'Kira',
@@ -21,6 +19,60 @@ new_form = Form.create(form_params) do |form|
       section: section,
       style: Question.styles[:input],
       title: 'Last Name',
+    )
+    Question.create(
+      key: 'preferred_name',
+      placeholder: 'Kira',
+      section: section,
+      style: Question.styles[:input],
+      title: 'Preferred Name',
+    )
+    Question.create(
+      key: 'birthday',
+      placeholder: '01/01/96',
+      section: section,
+      style: Question.styles[:input],
+      title: 'Birthday',
+    )
+    Question.create(
+      key: 'gender',
+      placeholder: 'Female',
+      section: section,
+      style: Question.styles[:input],
+      title: 'Gender',
+    )
+    Question.create(
+      key: 'cell_phone',
+      placeholder: '(555) 555-5555',
+      section: section,
+      style: Question.styles[:input],
+      title: 'Cell Phone',
+    )
+    Question.create(
+      key: 'home_phone',
+      placeholder: '(555) 555-5555',
+      section: section,
+      style: Question.styles[:input],
+      title: 'Home Phone',
+    )
+    Question.create(
+      key: 'home_address',
+      placeholder: '213 Queen Street',
+      section: section,
+      style: Question.styles[:input],
+      title: 'Home Address',
+    )
+  end
+  Section.create(
+    form: form,
+    title: 'Parent/Guardian Information',
+  ) do |section|
+    Question.create(
+      key: 'guardian_one_name',
+      placeholder: 'Kira',
+      section: section,
+      style: Question.styles[:input],
+      title: 'First Name',
     )
   end
 end
