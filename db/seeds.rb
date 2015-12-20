08ff2dcedd047daf66d358ab50c5bf1b399120f9
warrenshen <warrenzshen@gmail.com>
2015-12-19 21:25:42 -0800

    Explicitly define seed order in seeds controller module

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,15 @@
-def load_from_folder(folder)
-  Dir[File.join(Rails.root, 'db', 'seeds', folder, '*.rb')].sort.each do |seed|
-    puts "Seeding #{seed}."
-    load seed
-  end
-end
-
 if Rails.env.development? || Rails.env.staging?
-  load_from_folder('development')
+  seeds = [
+    'users',
+    'conferences',
+    'schools',
+    'students',
+    'comments',
+    'responsibilities',
+    'groups',
+    'forms',
+  ]
+  seeds.each do |seed|
+    load File.join(Rails.root, 'db/seeds/development', "#{seed}_seed.rb")
+  end
 end
