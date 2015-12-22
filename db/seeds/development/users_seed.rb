6d392a14bfc9b69f2842fa41faa64ce3ec7ccc10
warrenshen <warrenzshen@gmail.com>
2015-12-21 22:23:22 -0800

    Set up user validation of has_sidebar and is_admin

--- db/seeds/development/users_seed.rb
+++ db/seeds/development/users_seed.rb
@@ -1,10 +1,10 @@
 new_admin = User.new(
   email: 'admin@edgeyl.org',
   first_name: Faker::Name.first_name,
+  is_admin: true,
   last_name: Faker::Name.last_name,
   password: 'password',
   password_confirmation: 'password',
-  is_admin: true,
 )
 new_admin.skip_confirmation!
 new_admin.save
