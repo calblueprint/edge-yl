59cef879b4eea31afa29b42eb5df18e4662a3f41
warrenshen <warrenzshen@gmail.com>
2015-12-24 15:39:24 +0800

    Refactor comments controller and comment serializers

--- app/serializers/student_show_serializer.rb
+++ app/serializers/student_show_serializer.rb
@@ -6,7 +6,7 @@ class StudentShowSerializer < StudentIndexSerializer
              :guardian_two_name, :guardian_two_phone, :guardian_two_email,
              :home_phone, :preferred_name, :shirt_size
 
-  has_many :comments, serializer: CommentIndexSerializer
+  has_many :comments, serializer: CommentBaseSerializer
 
   has_one :group, serializer: GroupStudentSerializer
   has_one :responsibility, serializer: ResponsibilityStudentSerializer
