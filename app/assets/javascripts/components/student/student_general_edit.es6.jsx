1fd7185818aca19bcd0d29605a87fb5de692b7a6
warrenshen <warrenzshen@gmail.com>
2015-12-29 21:58:24 +0800

    Update student general edit inputs

--- app/assets/javascripts/components/student/student_general_edit.es6.jsx
+++ app/assets/javascripts/components/student/student_general_edit.es6.jsx
@@ -30,17 +30,18 @@ class StudentGeneralEdit extends Component {
           action={this.generateHandler('first_name')}
           errors={template.errors.first_name}
           margin={false}
-          placeholder={'First name'}
           value={template.first_name} />
+        <CardInput
+          action={this.generateHandler('preferred_name')}
+          errors={template.errors.preferred_name}
+          value={template.preferred_name} />
         <CardInput
           action={this.generateHandler('last_name')}
           errors={template.errors.last_name}
-          placeholder={'Last name'}
           value={template.last_name} />
         <CardInput
           action={this.generateHandler('birthday')}
           errors={template.errors.birthday}
-          placeholder={'Birthday'}
           value={template.birthday} />
       </div>
     );
