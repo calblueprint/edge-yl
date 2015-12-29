42cd4926d94f72a900d7896fea95215076d30423
Unzi Park <upark.go@gmail.com>
2015-12-28 19:30:51 -0800

    Update annotations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,12 +2,13 @@
 #
 # Table name: comments
 #
-#  id         :integer          not null, primary key
-#  content    :string           not null
-#  student_id :integer
-#  user_id    :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id               :integer          not null, primary key
+#  content          :string           not null
+#  commentable_id   :integer
+#  commentable_type :string
+#  user_id          :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
 #
 
 class Comment < ActiveRecord::Base
