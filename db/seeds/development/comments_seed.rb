38edb91378248559e1d7092aa19415562d9d665d
warrenshen <warrenzshen@gmail.com>
2015-12-30 21:37:52 +0800

    Create comments for schools before students

--- db/seeds/development/comments_seed.rb
+++ db/seeds/development/comments_seed.rb
@@ -7,14 +7,14 @@ def generate_comment(commentable_id, commentable_type)
   )
 end
 
-Student.all.each do |student|
-  generate_comment(student.id, 'Student')
-  generate_comment(student.id, 'Student')
-  puts "Created comments for #{student.full_name}."
-end
-
 School.all.each do |school|
   generate_comment(school.id, 'School')
   generate_comment(school.id, 'School')
   puts "Created comments for #{school.name} High School."
+end
+
+Student.all.each do |student|
+  generate_comment(student.id, 'Student')
+  generate_comment(student.id, 'Student')
+  puts "Created comments for #{student.full_name}."
 end
