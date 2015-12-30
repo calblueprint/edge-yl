45ed4bc11cf32688a1e0ed756637c4c4d4fd104c
warrenshen <warrenzshen@gmail.com>
2015-12-30 21:36:57 +0800

    Alphabetize column modifiers in create comments

--- db/migrate/20151106071352_create_comments.rb
+++ db/migrate/20151106071352_create_comments.rb
@@ -4,7 +4,7 @@ class CreateComments < ActiveRecord::Migration
     create_table :comments do |t|
       t.string :content, null: false
 
-      t.references :commentable, polymorphic: true, index: true
+      t.references :commentable, index: true, polymorphic: true
       t.references :user, index: true
 
       t.timestamps null: false
