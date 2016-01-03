115da986964e636e694e2349e43b515575b95411
warrenshen <warrenzshen@gmail.com>
2016-01-03 13:32:04 -0800

    Include student gender choices in serialization

--- app/serializers/student_show_serializer.rb
+++ app/serializers/student_show_serializer.rb
@@ -2,16 +2,21 @@ class StudentShowSerializer < StudentIndexSerializer
 
   attributes :address_city, :address_one, :address_state,
              :address_two, :address_zip, :birthday, :first_name,
-             :gender, :guardian_email, :guardian_employer,
-             :guardian_first_name, :guardian_job_title,
-             :guardian_last_name, :guardian_phone_number,
-             :guardian_phone_type, :guardian_relationship,
-             :home_phone, :last_name, :preferred_name, :shirt_size
+             :gender, :gender_choices, :guardian_email,
+             :guardian_employer, :guardian_first_name,
+             :guardian_job_title, :guardian_last_name,
+             :guardian_phone_number, :guardian_phone_type,
+             :guardian_relationship, :home_phone, :last_name,
+             :preferred_name, :shirt_size
 
   has_many :comments, serializer: CommentBaseSerializer
 
   def gender
     object.gender.humanize
+  end
+
+  def gender_choices
+    Student.genders.keys.map { |gender| gender.humanize }
   end
 
   def guardian_phone_type
