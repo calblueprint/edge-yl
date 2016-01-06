761830a4253629c0b840cf3296d97e43020ed9c8
warrenshen <warrenzshen@gmail.com>
2016-01-06 11:29:52 -0800

    Configure initial emails controller strong params

--- app/controllers/api/emails_controller.rb
+++ app/controllers/api/emails_controller.rb
@@ -3,12 +3,30 @@ class Api::EmailsController < Api::BaseController
   skip_before_filter :authenticate_user
 
   def create
-    email = Email.new params
+    puts params
+    custom_params = ActionController::Parameters.new(
+      content: params['body-plain'],
+      receiver: params[:recipient],
+      sender: params[:from],
+      subject: params[:subject],
+    )
+    email = Email.new email_params(custom_params)
     if email.save
       render json: { message: 'Received' }, status: :ok
     else
       unprocessable_response email
     end
+  end
+
+  private
+
+  def email_params(params)
+    params.permit(
+      :content,
+      :receiver,
+      :sender,
+      :subject,
+    )
   end
 
 end
