931370fa9028cacc682b7fe7e8db0058c59d9db3
warrenshen <warrenzshen@gmail.com>
2016-01-11 19:51:06 -0800

    Fix client base controller authenticate_user filter

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -3,9 +3,12 @@ class BaseController < ApplicationController
   before_filter :authenticate_user
 
   def authenticate_user
-    redirect_to login_path if !user_signed_in?
-    profile_json = ProfileBaseSerializer.new(current_user).to_json
-    @profile = JSON.parse(profile_json)['user']
+    if !user_signed_in?
+      redirect_to login_path
+    else
+      profile_json = ProfileBaseSerializer.new(current_user).to_json
+      @profile = JSON.parse(profile_json)['user']
+    end
   end
 
 end
