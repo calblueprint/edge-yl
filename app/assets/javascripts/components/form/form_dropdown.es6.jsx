765d3f888c11f8eb2cc35fca09b327be3fb03326
warrenshen <warrenzshen@gmail.com>
2016-01-26 23:44:13 -0800

    Refactor form dropdown component with dropdown button

--- app/assets/javascripts/components/form/form_dropdown.es6.jsx
+++ app/assets/javascripts/components/form/form_dropdown.es6.jsx
@@ -10,16 +10,6 @@ class FormDropdown extends Component {
   }
 
   // --------------------------------------------------
-  // State
-  // --------------------------------------------------
-  static get defaultState() {
-    return {
-      dropdown: false,
-      value: 'Select one',
-    };
-  }
-
-  // --------------------------------------------------
   // Styles
   // --------------------------------------------------
   get styles() {
@@ -34,94 +24,36 @@ class FormDropdown extends Component {
         paddingLeft: '4px',
         color: StyleConstants.colors.red,
       },
-      section: {
-        display: 'flex',
-        flexFlow: 'column',
-        flex: '1',
-        height: '34px',
-        marginLeft: '24px',
-        border: `1px solid ${StyleConstants.colors.gray}`,
-      },
-    };
-  }
-
-  get clickableStyles() {
-    return {
-      child: {
-        paddingRight: '4px',
-      },
-      default: {
-        display: 'flex',
-        alignItems: 'center',
-        flex: '1',
-        minHeight: '34px',
-        padding: '8px',
-        boxSizing: 'border-box',
-      },
-    };
-  }
-
-  get dropdownStyles() {
-    return {
-      child: {
-        default: {
-          flex: '1',
-          padding: '8px',
-        },
-        hover: {
-          backgroundColor: StyleConstants.colors.turquoise,
-        },
-      },
-      container: Object.assign(
-        {},
-        StyleConstants.containers.card,
-        {
-          top: '6px',
-          left: '0px',
-          zIndex: StyleConstants.planes.two,
-        }
-      ),
     };
   }
 
   // --------------------------------------------------
   // Helpers
   // --------------------------------------------------
-  generateOption(option) {
+  generateChoice(choice) {
     return {
-      action: () => this.storeResponse(option),
-      content: option,
+      action: () => this.storeResponse(choice),
+      content: choice,
     };
   }
 
-  generateOptions() {
+  generateChoices() {
     var options = this.props.question.options;
-    return options.map((option) => this.generateOption(option));
+    return options.map((choice) => this.generateChoice(choice));
   }
 
-  storeResponse(option) {
+  storeResponse(choice) {
     var question = this.props.question;
     FormActions.storeResponse(
       question.section_id,
       question.id,
-      option
+      choice
     );
   }
 
   // --------------------------------------------------
   // Render
   // --------------------------------------------------
-  renderDropdown() {
-    if (this.state.dropdown) {
-      return (
-        <Dropdown
-          action={() => this.setState({ dropdown: false })}
-          options={this.generateOptions()}
-          styles={this.dropdownStyles} />
-      );
-    }
-  }
-
   renderRequired() {
     if (this.props.question.is_required) {
       return <h6 style={this.styles.required}>{'*'}</h6>;
@@ -134,18 +66,9 @@ class FormDropdown extends Component {
       <div style={this.styles.container}>
         <h5>{question.title}</h5>
         {this.renderRequired()}
-        <div style={this.styles.section}>
-          <Clickable
-            action={() => this.setState({ dropdown: true })}
-            icon={TypeConstants.icons.expand}
-            styles={this.clickableStyles}
-            type={'i'}>
-            <h6 style={this.styles.selected}>
-              {question.value || 'Select one'}
-            </h6>
-          </Clickable>
-          {this.renderDropdown()}
-        </div>
+        <DropdownButton
+          choices={this.generateChoices()}
+          value={question.value} />
       </div>
     );
   }
