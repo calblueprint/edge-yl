35535f7fbe61435b3009bffb028b73cc631d3e1f
warrenshen <warrenzshen@gmail.com>
2016-02-22 23:51:00 -0800

    Configure create pages migration to run earlier

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210042524) do
+ActiveRecord::Schema.define(version: 20160210032502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
