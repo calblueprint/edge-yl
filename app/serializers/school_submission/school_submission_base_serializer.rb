a14b96b3c14df58c34c56b02449470d09448f3f4
warrenshen <warrenzshen@gmail.com>
2016-02-24 00:53:19 -0800

    Create school and student submission serializers

---
+++ app/serializers/school_submission/school_submission_base_serializer.rb
@@ -0,0 +1,7 @@
+class SchoolSubmissionBaseSerializer < BaseSerializer
+
+  attributes :id, :address_city, :address_one, :address_state,
+             :address_two, :address_state, :address_two, :address_zip,
+             :current_page, :name, :uuid, :website
+
+end
