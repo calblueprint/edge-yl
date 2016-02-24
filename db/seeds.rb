8523a4e55a639e02265aed66c77a77918b9f54e0
warrenshen <warrenzshen@gmail.com>
2016-02-23 23:29:12 -0800

    Fix seeds controller file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,3 @@
-file = File.read('./db/data/Selection form - 1.csv')
-if file
-  csv_text = file
-  csv = CSV.parse(csv_text, headers: true)
-  csv.each do |row|
-    School.create! address_city: row[5],
-                   address_one: row[6],
-                   address_two: row[8],
-                   address_state: row[9],
-                   address_zip: row[10],
-                   name: row[1]                    
-  end
-end
-
 if Rails.env.development? || Rails.env.staging?
   seeds = [
     'users',
@@ -28,5 +14,18 @@ if Rails.env.development? || Rails.env.staging?
   ]
   seeds.each do |seed|
     load File.join(Rails.root, 'db/seeds/development', "#{seed}_seed.rb")
+  end
+end
+
+if File.exist?('./db/data/Selection form - 1.csv')
+  file = File.read('./db/data/Selection form - 1.csv')
+  csv = CSV.parse(file, headers: true)
+  csv.each do |row|
+    School.create! address_city: row[5],
+                   address_one: row[6],
+                   address_two: row[8],
+                   address_state: row[9],
+                   address_zip: row[10],
+                   name: row[1]
   end
 end
