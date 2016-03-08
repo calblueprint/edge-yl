cb662c0af736a961c717968377399b19b1a73eb2
Anthony <anthonyhbh@gmail.com>
2016-03-07 22:55:04 -0800

    Add import page
    Test Plan: none
    
    Reviewers: warren
    
    Subscribers: qromanek, trickywolffe, unzunz, sonia
    
    Differential Revision: http://phab.calblueprint.org/D337

---
+++ app/assets/javascripts/components/import/import_page.es6.jsx
@@ -0,0 +1,89 @@
+class ImportPage extends Component {
+
+  // --------------------------------------------------
+  // Setup
+  // --------------------------------------------------
+  constructor(props) {
+    super(props);
+    this._listener = (state) => this.setState(state);
+  }
+
+  // --------------------------------------------------
+  // Props
+  // --------------------------------------------------
+  static get propTypes() {
+    return {
+      profile: React.PropTypes.object.isRequired,
+    };
+  }
+
+  // --------------------------------------------------
+  // Styles
+  // --------------------------------------------------
+  get styles() {
+    return {
+      container: {
+        display: 'flex',
+        flexFlow: 'column',
+        alignItems: 'center',
+        paddingTop: StyleConstants.heights.header,
+      },
+      footer: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'flex-end',
+        alignSelf: 'stretch',
+        margin: '12px 0px',
+      },
+      label: {
+        paddingRight: '4px',
+      },
+    };
+  }
+
+  // --------------------------------------------------
+  // Lifecycle
+  // --------------------------------------------------
+  componentWillMount() {
+    this.setState(ProfileStore.getState());
+    this.setState(FeedbackStore.getState());
+  }
+
+  componentDidMount() {
+    ProfileStore.listen(this._listener);
+    FeedbackStore.listen(this._listener);
+  }
+
+  componentWillUnmount() {
+    ProfileStore.unlisten(this._listener);
+    FeedbackStore.unlisten(this._listener);
+  }
+
+  // --------------------------------------------------
+  // Helpers
+  // --------------------------------------------------
+  selectProfile() {
+    return this.state.profile ?
+           this.state.profile :
+           this.props.profile;
+  }
+
+  // --------------------------------------------------
+  // Render
+  // --------------------------------------------------
+  render() {
+    return (
+      <div style={StyleConstants.pages.wrapper}>
+        <Header profile={this.selectProfile()} />
+        <Sidebar profile={this.selectProfile()} />
+        <div style={StyleConstants.pages.container}>
+          <div style={StyleConstants.pages.content}>
+            <ImportCard
+              profile={this.props.profile}
+              template={this.state.template} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
