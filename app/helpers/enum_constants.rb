2c1cec3becb1f5c8a6295689ab597b3a097ae9a4
warrenshen <warrenzshen@gmail.com>
2016-03-21 12:15:45 -0700

    Set up custom validation error messages

--- app/helpers/enum_constants.rb
+++ app/helpers/enum_constants.rb
@@ -4,12 +4,22 @@ module EnumConstants
 
   DIETARY_RESTRICTIONS = %w(dairy-free gluten-free none nut-allergy vegan vegetarian)
 
+  EMAIL_FORMAT = {
+    message: 'should follow format x@x.x',
+    with: /.+@.+\..+/,
+  }
+
   GENDERS = %w(female male other)
 
   GUARDIAN_RELATIONSHIPS = %w(
   	mother father aunt uncle
   	grandmother grandfather
   	stepmother stepfather guardian)
+
+  PHONE_FORMAT = {
+    message: 'should follow format ###-###-####',
+    with: /\A\d{3}-\d{3}-\d{4}\z/,
+  }
 
   PHONE_TYPES = %w(cell home work)
 
@@ -22,4 +32,10 @@ module EnumConstants
     NM NY NC ND OH OK OR PA RI SC
     SD TN TX UT VT VA WA WV WI WY
   )
+
+  ZIP_FORMAT = {
+    message: 'should follow format ##### or #####-####',
+    with: /\A\d{5}(-\d{4})?\z/,
+  }
+
 end
