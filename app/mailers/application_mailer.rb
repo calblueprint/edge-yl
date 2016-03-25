17fa5dac1175b90962c779425447df45e4191999
warrenshen <warrenzshen@gmail.com>
2016-03-25 14:06:11 -0700

    Refactor mailers infrastructure

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,24 @@
 class ApplicationMailer < ActionMailer::Base
 
+  layout 'mailer'
+
   default from: ENV['smtp_username']
   default to: ENV['test_email']
-  layout 'mailer'
+
+  def standard(email)
+    @email = email
+    if Rails.env.staging?
+      mail from: email.from,
+           recipient: email.recipient,
+           sender: email.sender,
+           subject: email.subject,
+           to: email.to
+    else
+      mail from: email.from,
+           to: email.to,
+           subject: email.subject,
+           template_path: 'mails'
+    end
+  end
 
 end
--- spec/mailers/previews/school_mailer_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/school_mailer
-class SchoolMailerPreview < ActionMailer::Preview
-
-end
