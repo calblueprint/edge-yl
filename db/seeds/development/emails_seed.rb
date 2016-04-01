5441942caf98e39ec17faf9a2365883a3078b23a
warrenshen <warrenzshen@gmail.com>
2016-04-01 13:41:45 -0700

    Uncomment emails seed since they're working again

--- db/seeds/development/emails_seed.rb
+++ db/seeds/development/emails_seed.rb
@@ -1,30 +1,30 @@
-# admin = User.first
+admin = User.first
 
-# (1..10).each do |index|
-#   if [true,false].sample
-#     school = School.offset(rand(School.count)).first
-#     email = school.primary_contact.email
-#     id = school.id
-#     name = school.primary_contact.full_name
-#     type = School.name
-#   else
-#     student = Student.offset(rand(Student.count)).first
-#     email = student.email
-#     id = student.id
-#     name = student.full_name
-#     type = Student.name
-#   end
-#   new_email = Email.create(
-#     content: Faker::Lorem.sentence,
-#     emailable_id: id,
-#     emailable_type: type,
-#     from: "#{name} <#{email}>",
-#     is_sent: true,
-#     recipient: "#{admin.username}@test.edge.org",
-#     sender: email,
-#     subject: "#{Faker::Hacker.noun} #{Faker::Hacker.verb} #{Faker::Hacker.noun}".humanize,
-#     to: "#{admin.full_name} <#{admin.username}@test.edge.org>",
-#     user: admin,
-#   )
-#   puts "Create email from admin #{new_email.from}."
-# end
+(1..10).each do |index|
+  if [true,false].sample
+    school = School.offset(rand(School.count)).first
+    email = school.primary_contact.email
+    id = school.id
+    name = school.primary_contact.full_name
+    type = School.name
+  else
+    student = Student.offset(rand(Student.count)).first
+    email = student.email
+    id = student.id
+    name = student.full_name
+    type = Student.name
+  end
+  new_email = Email.create(
+    content: Faker::Lorem.sentence,
+    emailable_id: id,
+    emailable_type: type,
+    from: "#{name} <#{email}>",
+    is_sent: true,
+    recipient: "#{admin.username}@test.edge.org",
+    sender: email,
+    subject: "#{Faker::Hacker.noun} #{Faker::Hacker.verb} #{Faker::Hacker.noun}".humanize,
+    to: "#{admin.full_name} <#{admin.username}@test.edge.org>",
+    user: admin,
+  )
+  puts "Create email from admin #{new_email.from}."
+end
