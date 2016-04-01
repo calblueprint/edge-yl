5c5b03db36c1bfb1ac1232b26b3ddeb8502d07fc
Sonia Yang <sonia.yang@berkeley.edu>
2016-03-31 22:04:21 -0700

    Fix spacing

--- app/serializers/question/question_base_serializer.rb
+++ app/serializers/question/question_base_serializer.rb
@@ -1,7 +1,7 @@
 class QuestionBaseSerializer < BaseSerializer
 
   attributes :id,
-  			 :description,
+             :description,
              :format,
              :is_required,
              :key,
