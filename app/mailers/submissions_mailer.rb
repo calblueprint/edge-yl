45c4579428da8188f9d484d5f1ff41745bee882c
warrenshen <warrenzshen@gmail.com>
2016-04-07 20:15:13 -0700

    Create create parent html view

--- app/mailers/submissions_mailer.rb
+++ app/mailers/submissions_mailer.rb
@@ -3,30 +3,33 @@ class SubmissionsMailer < ApplicationMailer
   def create_student(student_submission)
     @student_submission = student_submission
     name = student_submission.full_name
-    mail to: student_submission.email,
-         subject: "EDGE Student Registration | #{name}",
-         template_path: 'mails'
+    mail subject: "EDGE Student Registration | #{name}",
+         template_path: 'mails',
+         to: student_submission.email
   end
 
   def create_parent(student_submission)
     @student_submission = student_submission
-    # mail to: student_submission.
+    name = student_submission.full_name
+    mail subject: "EDGE Student Registration | #{name}",
+         template_path: 'mails',
+         to: student_submission.guardian_one_email
   end
 
   def submit_school(school_submission)
     @school_submission = school_submission
     name = school_submission.name
-    mail to: @school_submission.contact_email,
-         subject: "EDGE School Registration Confirmation | #{name}",
-         template_path: 'mails'
+    mail subject: "EDGE School Registration Confirmation | #{name}",
+         template_path: 'mails',
+         to: @school_submission.contact_email
   end
 
   def submit_student(student_submission)
     @student_submission = student_submission
     name = student_submission.full_name
-    mail to: @student_submission.email,
-         subject: "EDGE Student Registration Confirmation | #{name}",
-         template_path: 'mails'
+    mail subject: "EDGE Student Registration Confirmation | #{name}",
+         template_path: 'mails',
+         to: @student_submission.email
   end
 
 end
