0a7185f65d4b618154e463c1df4979ff5113c5cc
Sonia Yang <sonia.yang@berkeley.edu>
2016-04-12 03:42:14 -0700

    Create dependencies between questions on form

--- app/assets/javascripts/components/form/form_page.es6.jsx
+++ app/assets/javascripts/components/form/form_page.es6.jsx
@@ -34,37 +34,49 @@ class FormPage extends Component {
   // Render
   // --------------------------------------------------
   renderQuestion(question) {
-    switch (question.style) {
-      case 'dropdown':
-        return (
-          <FormDropdown
-            key={question.id}
-            question={question} />
-        );
-      case 'information':
-        return (
-          <FormInformation
-            key={question.id}
-            question={question} />
-        );
-      case 'input':
-        return (
-          <FormInput
-            key={question.id}
-            question={question} />
-        );
-      case 'textarea':
-        return (
-          <FormTextarea
-            key={question.id}
-            question={question} />
-        );
-      case 'waiver':
-        return (
-          <FormWaiver
-            key={question.id}
-            question={question} />
-        );
+    var questions = this.props.page.questions;
+    var render = true;
+    if (question.enabler_key) {
+      for (var i = 0; i < questions.length; i++) {
+        if (question.enabler_key == questions[i].key && question.enabler_value != questions[i].value) {
+          render = false;
+          break;
+        }
+      }
+    }
+    if (render) {
+      switch (question.style) {
+        case 'dropdown':
+          return (
+            <FormDropdown
+              key={question.id}
+              question={question} />
+          );
+        case 'information':
+          return (
+            <FormInformation
+              key={question.id}
+              question={question} />
+          );
+        case 'input':
+          return (
+            <FormInput
+              key={question.id}
+              question={question} />
+          );
+        case 'textarea':
+          return (
+            <FormTextarea
+              key={question.id}
+              question={question} />
+          );
+        case 'waiver':
+          return (
+            <FormWaiver
+              key={question.id}
+              question={question} />
+          );
+      }
     }
   }
 
