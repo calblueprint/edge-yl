0c1e0051bf4303af4c634b7029fbee83c4344414
warrenshen <warrenzshen@gmail.com>
2016-04-27 22:22:15 -0700

    Configure volunteers factory

--- spec/factories/volunteers.rb
+++ spec/factories/volunteers.rb
@@ -12,7 +12,8 @@
 
 FactoryGirl.define do
   factory :volunteer do
-    
+    email       { Faker::Internet.email }
+    first_name  { Faker::Name.first_name }
+    last_name   { Faker::Name.last_name }
   end
-
 end
