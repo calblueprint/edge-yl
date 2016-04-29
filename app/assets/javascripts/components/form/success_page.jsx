38b75ba41edd65b19ded7a2e919075a9d2bc1765
Sonia Yang <sonia.yang@berkeley.edu>
2016-04-29 13:30:36 -0700

    Modify success page text specific to school/student form

--- app/assets/javascripts/components/form/success_page.jsx
+++ app/assets/javascripts/components/form/success_page.jsx
@@ -34,31 +34,63 @@ class SuccessPage extends Component {
   // --------------------------------------------------
   // Render
   // --------------------------------------------------
+  renderText() {
+    var target = this.props.target;
+    if (target == 'school') {
+      return (
+         <div style={this.styles.container}>
+          <i style={this.styles.icon} className="fa fa-check-circle fa-5x"></i>
+          <h3>{`You have successfully completed the school form for EDGE!`}</h3>
+          <br />
+          <p>{`
+            You should be receiving a confirmation email to the email address provided
+            shortly. Your student will also be receiving instructions on how to confirm/finish
+            their registration via that email. We may ask for your assistance in helping the student
+            confirm their registration.
+            `}
+          </p>
+          <br />
+          <p>{`
+            Questions? Please contact us at registration@edgeyl.org. You may also leave us
+            a message at (510) 408-6606, but please note that we generally response much faster
+            to emails`}
+          </p>
+          <br />
+          <br />
+          <a href='http://www.edgeyl.org/'><h4>Click here to go back to the EDGE website</h4></a>
+        </div>
+      )
+    } else if (target == 'student') {
+      return (
+         <div style={this.styles.container}>
+          <i style={this.styles.icon} className="fa fa-check-circle fa-5x"></i>
+          <h3>{`You have successfully completed the student form for EDGE!`}</h3>
+          <br />
+          <p>{`
+            You should be receiving a confirmation email to the email address provided
+            shortly. Your junior staff member will be calling you with more information as
+            we approach the conference. 
+            `}
+          </p>
+          <br />
+          <p>{`
+            Questions? Please contact us at registration@edgeyl.org. You may also leave us
+            a message at (510) 408-6606, but please note that we generally response much faster
+            to emails`}
+          </p>
+          <br />
+          <br />
+          <a href='http://www.edgeyl.org/'><h4>Click here to go back to the EDGE website</h4></a>
+        </div>
+      )
+    }
+  }
+  
   render() {
   	return (
       <div style={StyleConstants.wrappers.center}>
     		<div style={StyleConstants.pages.center}>
-          <div style={this.styles.container}>
-            <i style={this.styles.icon} className="fa fa-check-circle fa-5x"></i>
-      			<h3>{`You have successfully completed the ${this.props.target} form for EDGE!`}</h3>
-            <br />
-            <p>{`
-              You should be receiving a confirmation email to the email address provided
-              shortly. Your student will also be receiving instructions on how to confirm/finish
-              their registration via that email. We may ask for your assistance in helping the student
-              confirm their registration.
-              `}
-            </p>
-            <br />
-            <p>{`
-              Questions? Please contact us at registration@edgeyl.org. You may also leave us
-              a message at (510) 408-6606, but please note that we generally response much faster
-              to emails`}
-            </p>
-            <br />
-            <br />
-            <a href='http://www.edgeyl.org/'><h4>Go back to the EDGE website</h4></a>
-          </div>
+          {this.renderText()}
     		</div>
       </div>
   	);
