6c57d35e2dce861816e1fb7e02a94edbc016788e
Anthony <anthonyhbh@gmail.com>
2016-04-29 17:45:24 -0700

    Add student_submission feature and ported over column values, still fixing bugs
    Summary: <longer, detailed summary>
    
    Test Plan: none
    
    Reviewers: warren
    
    Reviewed By: warren
    
    Subscribers: qromanek, sonia, unzunz, trickywolffe, warren
    
    Projects: #edge-yl
    
    Differential Revision: http://phab.calblueprint.org/D556

--- app/controllers/api/imports_controller.rb
+++ app/controllers/api/imports_controller.rb
@@ -30,39 +30,44 @@ class Api::ImportsController < Api::BaseController
     # if student's shchool is a propect we delete prospect and make a school
     # if school is already there, append a new contact.
     file = params[:file]
+    render json: { message: 'What is good?' },
+           status: :created
     csv = CSV.parse(file.open, headers: true)
     csv.each do |row|
-      # school = School.create(
-      #  address_city: row[8],
-      #  address_one: row[6],
-      #  address_two: row[7],
-      #  address_state: row[9],
-      #  address_zip: row[10],
-      #  name: row[1],
-      # )
-      # contact = Contact.create(
-      #   email: row[11],
-      #   first_name: row[2],
-      #   is_primary: true,
-      #   last_name: row[3],
-      #   phone_number: row[12],
-      #   school: school,
-      #   title: row[4],
-      # )
-      Student.create(
+      school = School.create(
+       address_city: row[8],
+       address_one: row[6],
+       address_two: row[7],
+       address_state: row[9],
+       address_zip: row[10],
+       name: row[1],
+      )
+      contact = Contact.create(
+        email: row[11],
+        first_name: row[2],
+        is_primary: true,
+        last_name: row[3],
+        phone_number: row[12],
+        school: school,
+        title: row[4],
+      )
+      student_submission = StudentSubmission.create(
         address_city: row[22].blank? ? 'n/a' : row[22],
         address_one: row[20].blank? ? 'n/a' : row[20],
-        address_state: row[23].blank? ? 'n/a' : row[23],
+        address_state: 'CA',
         address_two: row[21].blank? ? 'n/a' : row[21],
         address_zip: row[24] .blank? ? 'n/a' : row[24],
-        allergies: EnumConstants::BOOLEANS.sample, # row55
+        allergies: EnumConstants::BOOLEANS.sample, #ROW55
+        allergies_other: 'None',
         birthday: '2001-12-31', # row29
+        carpool: EnumConstants::CARPOOL_OPTIONS.sample,
         cell_phone: row[27] || 'n/a',
-        conference_id: params[:conference_id].to_i,
+        ceremony_attendance: EnumConstants::CEREMONY_OPTIONS.sample,
+        ceremony_attendance_number: 999,
+        current_page: 3,
         dietary_restrictions: 0, # row70
-        other_dietary_restrictions: 'Nut allergy',
-        email: row[25] || 'n/a',
-        emergency_consent: 0,
+        email: row[26] || 'n/a',
+        emergency_consent: 'yes',
         exercise_limitations: 'None',
         first_name: row[17] || 'n/a',
         gender: EnumConstants::GENDERS.sample, # row28
@@ -72,22 +77,64 @@ class Api::ImportsController < Api::BaseController
         guardian_one_job_title: row[38] || 'n/a',
         guardian_one_last_name: row[32] || 'n/a',
         guardian_one_phone_number: row[36] || 'n/a',
-        guardian_one_phone_type: EnumConstants::PHONE_TYPES.sample, # row36
-        guardian_one_relationship: EnumConstants::GUARDIAN_RELATIONSHIPS.sample, # row33
-        health_conditions: EnumConstants::BOOLEANS.sample,
+        guardian_one_phone_type: EnumConstants::PHONE_TYPES.sample, #ROW36
+        guardian_one_relationship: EnumConstants::GUARDIAN_RELATIONSHIPS.sample, #ROW33
+        guardian_two_email: "",
+        guardian_two_employer: "",
+        guardian_two_first_name: "",
+        guardian_two_job_title: "",
+        guardian_two_last_name: "",
+        guardian_two_phone_number:  "",
+        guardian_two_phone_type:  "",
+        guardian_two_relationship:  "",
+        health_conditions: EnumConstants::HEALTH_CONDITIONS.sample,
         health_conditions_description: 'None',
         home_phone: row[26] || 'n/a',
         immunizations: EnumConstants::BOOLEANS.sample, # row54,
-        is_flagged: 0,
-        is_primary: 0,
+        insurance: EnumConstants::BOOLEANS.sample,
+        insurance_address: Faker::Address.street_address,
+        insurance_address_city: Faker::Address.city,
+        insurance_address_state: EnumConstants::STATES.sample,
+        insurance_address_zip: Faker::Address.zip,
+        insurance_id: Faker::Name.first_name,
+        insurance_phone_number: Faker::Base.numerify('###-###-####'),
+        insurance_provider: 'None',
+        insurance_other: 'None',
+        is_primary: true,
         last_name: row[18] || 'n/a',
-        medications: 'Warren',
-        preferred_name: 'Warren',
-        psychologist_consent: 0,
-        psychologist_consent_name: 'Doctor',
-        # school: school,
-        shirt_size: EnumConstants::SHIRT_SIZES.sample, # row30
+        medications: 'None',
+        other_dietary_restrictions: '',
+        participation_guardian_consent: EnumConstants::AGREEMENTS.sample,
+        participation_guardian_name: Faker::Name.first_name,
+        participation_student_consent: EnumConstants::AGREEMENTS.sample,
+        participation_student_name: Faker::Name.first_name,
+        preferred_name: Faker::Name.first_name,
+        psychologist_consent: EnumConstants::BOOLEANS.sample,
+        psychologist_consent_name: Faker::Name.first_name,
+        risk_guardian_consent: EnumConstants::AGREEMENTS.sample,
+        risk_guardian_date: Faker::Date.between(33.days.ago, Time.zone.today),
+        risk_guardian_email: Faker::Internet.email,
+        risk_guardian_name: Faker::Name.first_name,
+        risk_guardian_relationship: EnumConstants::GUARDIAN_RELATIONSHIPS.sample,
+        risk_student_consent: EnumConstants::AGREEMENTS.sample,
+        risk_student_date: Faker::Date.between(33.days.ago, Time.zone.today),
+        risk_student_email: Faker::Internet.email,
+        risk_student_name: Faker::Name.first_name,
+        shirt_size: EnumConstants::SHIRT_SIZES.sample,
+        transportation: EnumConstants::TRANSPORTATION_OPTIONS.sample,
+        transportation_arrival_date: Faker::Date.between(33.days.ago, Time.zone.today),
+        transportation_arrival_time: Time.zone.now,
+        transportation_carrier: 'Bart',
+        transportation_consent: EnumConstants::AGREEMENTS.sample,
+        transportation_consent_name: Faker::Name.first_name,
+        transportation_departure_date: Faker::Date.between(33.days.ago, Time.zone.today),
+        transportation_departure_time: Time.zone.now,
+        transportation_name: Faker::Name.first_name,
+        transportation_number: 777,
+        conference_id: params[:conference_id].to_i,
+        # add school attribue to student_submission.rb
       )
+      student_submission.seed_submission
       # -1, 'unknown'
       # if school exiists, don't create new one
     end
